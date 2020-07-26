edcdcff5d2b320fe018c1d528b00d3ce9172fc61
narisada014 <nari.reishi@gmail.com>
2020-07-27 04:44:40 +0900

    add unit_vector_func

--- vector.h
+++ vector.h
@@ -10,8 +10,44 @@ class vec3 {
         inline double x() const { return e[0]; }
         inline double y() const { return e[1]; }
         inline double z() const { return e[2]; }
+        inline double r() const { return e[0]; }
+        inline double g() const { return e[1]; }
+        inline double b() const { return e[2]; }
 
         inline const vec3& operator+() const { return *this; }
-    public:
+        inline vec3 operator-() const { return *this; }
+        inline double operator[](int i) const { return e[i]; }
+        inline double& operator[](int i) { return e[i]; };
+
+        inline vec3& operator+=(const vec3 &v2);
+        inline vec3& operator-=(const vec3 &v2);
+        inline vec3& operator*=(const vec3 &v2);
+        inline vec3& operator/=(const vec3 &v2);
+        inline vec3& operator*=(const double t);
+        inline vec3& operator/=(const double t);
+
+        inline double length() const {
+            return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
+        }
+        inline double suared_length() const {
+            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        }
+        inline void make_unit_vector();
         double e[3];
 };
+
+inline std::istream& operator>>(std::istream &is, vec3 &t) {
+    is >> t.e[0] >> t.e[1] >> t.e[2];
+    return is;
+}
+
+inline std::ostream& operator<<(std::ostream &os, const vec3 &t) {
+    os << t.e[0] << t.e[1] << t.e[2];
+    return os;
+}
+
+// 単位ベクトルの計算
+inline void vec3::make_unit_vector() {
+    double k = 1.0 / sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
+    e[0] *= k; e[1] *= k; e[2] *= k;
+}
